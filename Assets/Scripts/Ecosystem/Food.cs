a02139ef0105ccf2d283d8925bebfcdf23d6567d
Kheeto <kheetoxd@gmail.com>
2023-10-25 21:42:29 +0200

    Plant reproduction
    Plants will now spread seeds to generate new plants rather than regrowing after being eaten.

--- Assets/Scripts/Ecosystem/Food.cs
+++ Assets/Scripts/Ecosystem/Food.cs
@@ -4,30 +4,10 @@ public class Food : MonoBehaviour {
 
     [Header("Stats")]
     [SerializeField] private float hungerRestore = 3f;
-    [SerializeField] private float regrowthTime = 10f;
-
-    [Header("References")]
-    [SerializeField] private GameObject meshHolder;
-
-    public bool isGrown;
-
-    private void Awake()
-    {
-        Regrow();
-    }
 
     public void Eat(Animal animal)
     {
-        isGrown = false;
-        meshHolder.SetActive(false);
-        Invoke(nameof(Regrow), regrowthTime);
-
         animal.RestoreHunger(hungerRestore);
-    }
-
-    private void Regrow()
-    {
-        meshHolder.SetActive(true);
-        isGrown = true;
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Ecosystem/Plant.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Plant : MonoBehaviour
+{
+    [Header("Plant Characteristics")]
+    [SerializeField] private float reproduceTime = 20f;
+    [SerializeField] private float spreadRange = 10f;
+    [SerializeField] private int minimumSeeds = 0;
+    [SerializeField] private int maximumSeeds = 2;
+
+    [Header("References")]
+    [SerializeField] private GameObject plantPrefab;
+
+    private void Awake()
+    {
+        StartCoroutine(Reproduce());
+    }
+
+    private IEnumerator Reproduce()
+    {
+        yield return new WaitForSeconds(reproduceTime);
+
+        int seedAmount = Random.Range(minimumSeeds, maximumSeeds + 1);
+        for (int i = 0; i < seedAmount; i++)
+        {
+            Vector3 spawnPosition;
+            if (NavMeshUtility.RandomPoint(transform.position, spreadRange, out spawnPosition))
+            {
+                Instantiate(plantPrefab, spawnPosition, Quaternion.identity);
+            }
+        }
+
+        StartCoroutine(Reproduce());
+    }
+}
