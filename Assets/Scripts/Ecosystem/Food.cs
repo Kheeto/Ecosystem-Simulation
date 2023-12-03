a8a05f28259cacc022facbc8323f1459e9d54404
Kheeto <kheetoxd@gmail.com>
2023-12-03 16:04:16 +0100

    Food class
    The Plant class has been removed and regrowth has been implemented in the food class

--- Assets/Scripts/Ecosystem/Food.cs
+++ Assets/Scripts/Ecosystem/Food.cs
@@ -1,13 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour {
 
     [Header("Stats")]
     [SerializeField] private float hungerRestore = 3f;
+    [SerializeField] private float regrowthTime = 10f;
+    [SerializeField] private bool destroyWhenEaten;
+
+    [Header("References")]
+    [SerializeField] private GameObject meshHolder;
+
+    public bool isGrown;
+
+    private void Awake()
+    {
+        if (!isGrown) isGrown = true;
+    }
 
     public void Eat(Animal animal)
     {
         animal.RestoreHunger(hungerRestore);
-        Destroy(gameObject);
+
+        if (destroyWhenEaten) Destroy(gameObject);
+        else
+        {
+            meshHolder.SetActive(false);
+            isGrown = false;
+            StartCoroutine(Regrow());
+        }
+    }
+
+    private IEnumerator Regrow()
+    {
+        yield return new WaitForSeconds(regrowthTime);
+
+        meshHolder.SetActive(true);
+        isGrown = true;
     }
 }
